cf50909e03595c1a21cf49f1d560d482e595fc9b
Maria Aparecida <cidinhalima_12@hotmail.com>
2022-11-25 19:57:20 -0300

    first commit

---
+++ dom.js
@@ -0,0 +1,9 @@
+let projetos = document.querySelector('#projetos')
+projetos.addEventListener('click', function(e) {
+    console.log('projetos')
+})
+
+let post = document.querySelector('#post')
+post.addEventListener('click', function(e) {
+    console.log('post')
+});
